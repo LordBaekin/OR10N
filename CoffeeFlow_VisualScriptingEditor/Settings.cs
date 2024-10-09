bfde14b3bb5aa7dbe0c918865bd76bb10483166a
LordBaekin <coryswild@gmail.com>
2024-10-09 13:23:43 -0400

    Changes to icon within program

--- CoffeeFlow_VisualScriptingEditor/Settings.cs
+++ CoffeeFlow_VisualScriptingEditor/Settings.cs
@@ -10,11 +10,11 @@
         
         public Settings() {
             // // To add event handlers for saving and changing settings, uncomment the lines below:
-            //
-            // this.SettingChanging += this.SettingChangingEventHandler;
-            //
-            // this.SettingsSaving += this.SettingsSavingEventHandler;
-            //
+            
+             this.SettingChanging += this.SettingChangingEventHandler;
+            
+             this.SettingsSaving += this.SettingsSavingEventHandler;
+            
         }
         
         private void SettingChangingEventHandler(object sender, System.Configuration.SettingChangingEventArgs e) {
