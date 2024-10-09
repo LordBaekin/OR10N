22547a17667581327ff4dd764e6413a4584813fa
LordBaekin <coryswild@gmail.com>
2024-10-09 13:26:49 -0400

    Modifications to AssemblyInfo

--- CoffeeFlow_VisualScriptingEditor/Properties/AssemblyInfo.cs
+++ CoffeeFlow_VisualScriptingEditor/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CoffeeFlow")]
+[assembly: AssemblyTitle("OR10N")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CoffeeFlow")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("OR10N")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
